4a58d844412854ef3a9225550a41dd048623f908
michaelhyi <michaelyi2004@gmail.com>
2023-10-14 00:20:09 -0400

    feat(user_token): fetch user token on launch

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -3,12 +3,26 @@ import Sidebar from "./Sidebar";
 import { AiOutlineMail } from "react-icons/ai";
 import { AiOutlineBell } from "react-icons/ai";
 import { TbLogout } from "react-icons/tb";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Container: React.FC<Props> = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios(process.env.NEXT_PUBLIC_API_URL + "/user/" + token)
+      .then((res) => setUser(res.data))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <></>;
+
   return (
     <div className="flex">
       <Sidebar />
@@ -16,7 +30,7 @@ const Container: React.FC<Props> = ({ children }) => {
         <div className="flex items-center h-24 bg-white border-b-[1.5px] border-b-neutral-100">
           <input className="ml-16 border-[1.5px] border-b-neutral-200 h-10 w-[512px] rounded-lg" />
           <div className="flex flex-col text-sm text-right ml-24">
-            <div>John Doe</div>
+            <div>{user.firstName + " " + user.lastName}</div>
             <div className="font-bold">Radiologist</div>
           </div>
           <div className="text-xs border-b-neutral-100 border-[1px] p-4 rounded-lg ml-16">
